d1ff850329ac0548d37f756754f40d5481327842
bruce-mig <h180009t@hit.ac.zw>
2024-01-22 22:52:50 +0200

    autoscaling

--- main.tf
+++ main.tf
@@ -31,18 +31,23 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami = data.aws_ami.app_ami.id
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
+  
+  name     =  blog
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [ module.blog_sg.security_group_id ]
+  
+  image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
-  vpc_security_group_ids = [ module.blog_sg.security_group_id ]
-
-  tags = {
-    Name = "Learning Terraform"
-  }
 }
 
-module "alb" {
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
@@ -51,17 +56,17 @@ module "alb" {
   load_balancer_type = "application"
 
   vpc_id          = module.blog_vpc.vpc_id
-  subnets         = module.blog_vpc.subnets
+  subnets         = module.blog_vpc.public_subnets
   security_groups = [ module.blog_sg.security_group_id ]
 
   target_groups = [
     {
-      name_prefix = "blog-"
+      name_prefix         = "blog-"
       background_protocol = "HTTP"
-      backend_port = 80
-      target_type = "instance"
+      backend_port        = 80
+      target_type         = "instance"
       targets = {
-        my_target = {
+        my_target   = {
           target_id = aws_instance.blog.id
           port      = 80
         }
